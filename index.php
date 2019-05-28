9e7cfdee71b82edc38e69e16e543d12371aca0a9
MuchiruLewis <muchirulewis@gmail.com>
2019-05-28 19:34:54 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Facial</title>
+</head>
+<body>
+	<canvas class="output">
+		
+	</canvas>
+
+<script type="text/javascript">
+	function opencv_ready() {
+		console.log("opencv ready")
+	}
+
+</script>
+
+<script type="text/javascript" src="opencv.js" onload="opencv_ready()"></script>
+</body>
+</html>
